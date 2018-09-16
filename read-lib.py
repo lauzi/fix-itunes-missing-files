61f25d68c89cfd6f238e35c6dbb5b4c2f5932e34
LauZi <st61112@gmail.com>
2018-09-16 16:16:29 +0800

    Add finding file with most similiar path

--- read-lib.py
+++ read-lib.py
@@ -7,6 +7,9 @@ import pathlib
 import plistlib
 import functools
 
+# pip install editdistance
+import editdistance
+
 
 def open_library_xml(library_path):
     library_xml_path = library_path / 'iTunes Library.xml'
@@ -47,6 +50,15 @@ class Track(object):
     def __lt__(self, other):
         return self._compare_value < other._compare_value
 
+    def find_location(self, directory_tree):
+        if not self.location:
+            return ''
+
+        if self.file_exists():
+            return pathlib.Path(self.location)
+
+        return min(directory_tree, key=lambda p: editdistance.eval(self.location, p.as_posix()))
+
 
 def filter_missing_tracks(tracks):
     return [track for track in tracks if not track.file_exists()]
@@ -60,18 +72,28 @@ def print_missing_tracks(missing_tracks):
     print('Total: {}'.format(len(missing_tracks)))
 
 
+def get_directory_tree(library_root):
+    return list(pathlib.Path(library_root / 'iTunes Media' / 'Music').glob('**/*.mp3'))
+
+
 def main(args):
     if len(args) < 1:
         print('Call with library path!')
         return
 
     library_root = pathlib.Path(args[0]).expanduser().resolve()
+    directory_tree = get_directory_tree(library_root)
 
     library = open_library_xml(library_root)
     tracks = [Track(plist_object) for plist_object in library['Tracks'].values()]
 
     missing_tracks = filter_missing_tracks(tracks)
-    print_missing_tracks(missing_tracks)
+
+    missing_tracks.sort()
+    for track in missing_tracks:
+        actual_path = track.find_location(directory_tree)
+        if actual_path:
+            print('{} -> {}'.format(track.location, actual_path))
 
 
 if __name__ == '__main__':
